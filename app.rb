7e9698711e0ca73b66bc1f3250ceded29f09da43
github username <github email address>
2018-02-20 10:13:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
+
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
 	end
 
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
+# BREAK
+
+# get '/' do
+# 	erb :index
+# end
 
 end
